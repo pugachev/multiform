e6f5bb1bb6e16fee1930040eab74f9a421594bf0
池フクロウ <mtaketani37@gmail.com>
2022-03-27 23:11:33 +0900

    関数化しました

--- testpost.py
+++ testpost.py
@@ -12,6 +12,29 @@ PASS = "Manabu2010"
 OPTION="python"
 preUUID=''
 
+def datapost(session,file,row):
+
+    if file=='':
+        row[0]='complete'
+        row[1]='complete'
+    
+
+    headers = { "Content-Type": "multipart/form-data" }
+    request_data = {
+    "name":USER,
+    "password":PASS,
+    "title":row[0],
+    "body":row[1],
+    "category":row[3],
+    "headers":headers,
+    "back":"https://blog.ikefukuro40.tech/backend.php",
+    "mml_id":"0"
+    }
+
+    url_post = 'https://blog.ikefukuro40.tech/post.php'
+
+    res = session.post(url_post, files=file, data=request_data)
+
 # セッションを開始
 session = requests.session()
 
@@ -42,111 +65,24 @@ for row in f:
                 tgtpic='./tgtphtos/'+tmppic.strip()+'.jpg'
                 if os.path.isfile(tgtpic):
                     file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/jpeg'))]
+                    datapost(session,file,row)    
                 else:
                         tgtpic='./tgtphtos/'+tmppic.strip()+'.png'
                         if os.path.isfile(tgtpic):
                             file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/png'))]
-
-                dtitle=row[0]
-                
-                headers = { "Content-Type": "multipart/form-data" }
-                request_data = {
-                    "name":USER,
-                    "password":PASS,
-                    "title":row[0],
-                    "body":row[1],
-                    "category":row[3],
-                    "headers":headers,
-                    "back":"https://blog.ikefukuro40.tech/backend.php",
-                    "mml_id":"0"
-                }
-
-                url_post = 'https://blog.ikefukuro40.tech/post.php'
-
-                res = session.post(url_post, files=file, data=request_data)
-
-                print("debug0 "+tgtpic.strip())
-
+                            datapost(session,file,row)        
         else:
                 tgtpic='./tgtphtos/'+row[2].strip()+'.jpg'
                 # print(tgtpic)
                 if os.path.isfile(tgtpic):
-                    file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/jpeg'))]
-                    dtitle=row[0]
-                    
-                    headers = { "Content-Type": "multipart/form-data" }
-                    request_data = {
-                        "name":USER,
-                        "password":PASS,
-                        "title":row[0],
-                        "body":row[1],
-                        "category":row[3],
-                        "headers":headers,
-                        "back":"https://blog.ikefukuro40.tech/backend.php",
-                        "mml_id":"0"
-                    }
-
-                    url_post = 'https://blog.ikefukuro40.tech/post.php'
-
-                    res = session.post(url_post, files=file, data=request_data)
-
-                    print("debug0 "+tgtpic.strip())
-
-        # print(res)
+                     file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/jpeg'))]
+                     datapost(session,file,row)
         time.sleep(5)
     else:
         print("データ終了です")
         break
 
-print("debug1")
-file=''
-headers = { "Content-Type": "multipart/form-data" }
-request_data = {
-    "name":USER,
-    "password":PASS,
-    "title":'complete',
-    "body":'complete',
-    "category":'1',
-    "headers":headers,
-    "back":"https://blog.ikefukuro40.tech/backend.php",
-    "mml_id":"0"
-}
-
-url_post = 'https://blog.ikefukuro40.tech/post.php'
-
-res = session.post(url_post, files=file, data=request_data)
-print("debug2")
-
-
-
-
-
-
-
-
-# list = glob.glob('./tgtphtos/*')
-# for tgtfile in list:
-#     # file = [('image', ('takada.jpg', open('takada.jpg', 'rb'), 'image/jpeg'))]
-#     print(os.path.split(tgtfile)[1])
-#     file = [('image', (tgtfile, open(tgtfile, 'rb'), 'image/jpeg'))]
-
-#     headers = { "Content-Type": "multipart/form-data" }
-#     request_data = {
-#         "name":USER,
-#         "password":PASS,
-#         "title":"連続美人さん",
-#         "body":"美人さんを連続POSTしています",
-#         "category":"1",
-#         "headers":headers,
-#         "back":"https://blog.ikefukuro40.tech/backend.php",
-#         "mml_id":"0"
-#     }
-
-#     url_post = 'https://blog.ikefukuro40.tech/post.php'
-
-#     res = session.post(url_post, files=file, data=request_data)
-#     print(res)
-#     time.sleep(5)
+datapost(session,'',row)
 
 
  
